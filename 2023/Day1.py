f2ec62f433a96d0a5137ee78d2508e90b6ac73ab
Illango Zaruba <90343447+TimillaO3O@users.noreply.github.com>
2023-12-01 00:46:42 -0600

    Update Day1.py

--- 2023/Day1.py
+++ 2023/Day1.py
@@ -1,1 +1,21 @@
+data = open("data.txt").read().splitlines()
 
+part1 = 0
+part2 = 0
+for i in data:
+  p1 = ""
+  p2 = ""
+  for x in range(len(i)):
+    if i[x].isdigit():
+      p1 += i[x]
+      p2 += i[x]
+    for j, y in enumerate(["zero", "one", "two", 
+    "three", "four", "five", 
+    "six", "seven", "eight", "nine"]):
+      if i[x:].startswith(y):
+        p2 += str(j)
+  part1 += int(p1[0] + p1[-1])
+  part2 += int(p2[0] + p2[-1])
+
+print("PART 1: " + str(part1))
+print("PART 2: " + str(part2))
