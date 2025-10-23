6b24a04b5a309feb07def6051ae00acd5f37e157
Illango Zaruba <90343447+TimillaO3O@users.noreply.github.com>
2025-10-23 15:42:08 -0500

    Day 1, 2024 done

--- 2024/Day1.py
+++ 2024/Day1.py
@@ -1,1 +1,22 @@
-data = open("data.txt").read().splitlines()
+data = open("2024/data1.txt").read().split()
+
+leftSide = []
+rightSide = []
+for i in range(0, len(data), 2):
+    leftSide.append(data[i])
+    rightSide.append(data[i+1])
+
+leftSide.sort()
+rightSide.sort()
+
+sum = 0
+for i in range(0, len(leftSide)):
+    sum += abs(int(leftSide[i]) - int(rightSide[i]))
+
+print("Part 1: %d", sum)
+
+simScore = 0
+for i in leftSide:
+    simScore += int(i) * rightSide.count(i)
+
+print("Part 2: %d", simScore)
